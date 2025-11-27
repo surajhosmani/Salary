fafd6742ffd69b23b0de22a7ef3994b87399eeec
surajhosmani <surajhosmani83@gmail.com>
2025-11-27 14:24:44 +0530

    Create Salary.py

---
+++ Salary.py
@@ -0,0 +1,8 @@
+import sys
+if len(sys. argv) != 2:
+print("Usage: python salary_bonus py ‹sala! sys.exit()
+salary = float(sys.argv[1])
+bonus = salary * 0.10
+total = salary + bonus
+print("Bonus Amount:" print("Total Salary:"
+bonus) total)
